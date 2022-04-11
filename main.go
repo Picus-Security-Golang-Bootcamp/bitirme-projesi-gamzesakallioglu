8798c38a2503774b5d84ad3077df67690044aa28
gamzesakallioglu <gamzesakallioglu0@gmail.com>
2022-04-11 12:24:49 +0300

    add config files

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"log"
+
+	"github.com/gamze.sakallioglu/learningGo/bitirme-projesi-gamzesakallioglu/pkg/config"
 )
 
 func main() {
 	log.Println("Basket service is getting started")
+
+	// Setting environments
+	_, err := config.LoadConfig("./pkg/config/config-local")
+	if err != nil {
+		log.Fatalf("environment variables could not be set %v", err)
+	}
+	//
 
 }
